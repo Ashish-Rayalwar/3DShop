af0fdfe59031fc379daed51bd158811a0f7e8a0c
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-04-15 18:08:33 +0530

    new

---
+++ server/test.js
@@ -0,0 +1,16 @@
+// /api/files/download/jwtToken
+
+// file id
+
+// set userId with fileId to check user purchase
+
+// pegination
+// "engines" : {
+//     "npm" : ">=8.0.0 <9.0.0",
+//     "node" : ">=16.0.0 <17.0.0"
+//   },
+
+// "engines" : {
+//     "npm" : ">=8.0.0 <9.0.0",
+//     "node" : ">=16.0.0 <17.0.0"
+//   },
